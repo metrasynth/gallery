87038366d33b9d64dbcce62571acfc3c15eb37a7
Matthew Scott <matt@11craft.com>
2016-10-19 13:44:18 -0500

    Cursor timing in generators now makes better sense

--- drumloop1.py
+++ drumloop1.py
@@ -4,35 +4,57 @@ from s4ils import *
 @c.Generator.factory
 def bassdrummer(track):
     def kick():
-        cursor.drums.on(n.C5) | track | cursor
+        cursor.s.drums.on(n.C4) | track | cursor
+    cursor = first_cursor = yield
     while True:
+        beat, tick = cursor - first_cursor
+        if tick == 0:
+            kick()
+        if tick == 12 and beat % 16 == 15:
+            kick()
         cursor = yield
-        if cursor.tick == 0:
-            kick()
-        if cursor.tick == 12 and cursor.beat % 16 == 15:
-            kick()
 
 
 @c.Generator.factory
 def snaredrummer(track):
     def smack():
-        cursor.drums.on(n.G5) | track | cursor
+        cursor.s.drums.on(n.G5) | track | cursor
+    cursor = first_cursor = yield
     while True:
-        cursor = yield
-        if cursor.tick == 0 and cursor.beat % 4 % 2 == 1:
+        beat, tick = cursor - first_cursor
+        if tick == 0 and beat % 4 % 2 == 1:
             smack()
+        cursor = yield
 
 
 @c.Generator.factory
-def asciidrummer(track, pattern, note_map, ticks_per_step=6):
-    while True:
-        cursor = yield
-        if cursor.tick % ticks_per_step == 0:
-            pos = cursor.ticks // ticks_per_step % len(pattern)
-            char = pattern[pos]
-            if char in note_map:
-                module, note = note_map[char]
-                module.on(note) | track | cursor
+def asciidrummer(tracks, patterns, note_map, ticks_per_step=6, singleshot=False):
+    cursor = first_cursor = yield
+    finished = 0
+    if not isinstance(tracks, list):
+        tracks = [tracks]
+        patterns = [patterns]
+    while cursor:
+        beat, tick = cursor - first_cursor
+        ticks = beat * 24 + tick
+        if ticks % ticks_per_step == 0:
+            for track, pattern in zip(tracks, patterns):
+                p_len = len(pattern)
+                loops = ticks // ticks_per_step // p_len
+                pos = ticks // ticks_per_step % p_len
+                if not singleshot or loops == 0:
+                    char = pattern[pos]
+                    if char == '.':
+                        track.off() | cursor
+                    elif char in note_map:
+                        module, note = note_map[char]
+                        module.on(note) | track | cursor
+                elif singleshot and loops > 0 and pos == 0:
+                    finished += 1
+        if not singleshot or finished < len(patterns):
+            cursor = yield
+        else:
+            cursor = None
 
 
 s = Session()
@@ -46,13 +68,13 @@ with s[INIT]:
 with s[0, 0]:
     s.bass = bassdrummer(s.track(1)) | s
 
-with s[15, 0]:
+with s[14, 0]:
     s.snare = snaredrummer(s.track(2)) | s
 
-with s[16, 0]:
+with s[0, 0]:
     s.drummer = asciidrummer(
         s.track(3),
-        'c-c-' * 15 + 'cco-',
+        'cccc' * 15 + 'cco-',
         {
             'c': (s.drums, n.A6),
             'o': (s.drums, n.A5),
